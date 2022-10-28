3b4547cf09282735aff15d20bd3963f628c02c97
uditi-12 <72464340+uditi-12@users.noreply.github.com>
2022-10-29 01:56:24 +0530

    Create 206. Reverse Linked List.cpp

---
+++ LeetCode/206. Reverse Linked List.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL)
+            return NULL;
+        ListNode *prev=NULL,*curr=head,*nxt=head->next;
+        while(nxt!=NULL){
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+            nxt=nxt->next;
+        }
+        curr->next=prev;
+        return curr;
+    }
+};
