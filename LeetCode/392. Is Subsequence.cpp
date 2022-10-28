6d7b20271c2e79f24eed83c7fe616de6380f8953
uditi-12 <72464340+uditi-12@users.noreply.github.com>
2022-10-29 01:35:51 +0530

    Create 392. Is Subsequence.cpp

---
+++ LeetCode/392. Is Subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+        
+            for(int j=0;j<t.size();j++){
+                if(s[i]==t[j]){
+                    i++;
+                }
+            }
+            if(i==s.size()){
+                return true;
+            }
+        return false;
+           
+    }
+};
