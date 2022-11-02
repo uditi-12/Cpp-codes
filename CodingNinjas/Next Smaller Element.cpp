c63fc18145926baca7cf09773658e60d766cd160
uditi-12 <72464340+uditi-12@users.noreply.github.com>
2022-11-02 09:46:57 +0530

    Update Next Smaller Element.cpp

--- CodingNinjas/Next Smaller Element.cpp
+++ CodingNinjas/Next Smaller Element.cpp
@@ -15,3 +15,23 @@ vector<int> nextSmallerElement(vector<int> &arr, int n)
     }
     return arr;
 }
+
+//Approach 2
+#include<iostream>
+#include<bits/stdc++.h>
+
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    // Write your code here.
+    
+    stack<int> s;
+    vector<int>ans(n);
+    s.push(-1);
+    for(int i=n-1;i>=0;i--){
+        while(s.top()>=arr[i])
+            s.pop();
+        ans[i]=s.top();
+        s.push(arr[i]);
+    }
+    return ans;
+}
